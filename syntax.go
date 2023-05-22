4f769ff4c6e0f6829a6a0a51705f88d9ea16f500
sdreger <sdreger@datenbankgesellschaft.de>
2023-05-22 10:17:30 +0200

    more basic syntax

--- syntax.go
+++ syntax.go
@@ -5,16 +5,21 @@ import (
 	"os"
 )
 
-/*
-var number int       //decalaration must be typed, value is 0
-var firstname string //value is ""
-var number2 = 2      //assigned - no need to write down type
-var name = "James Kirk"
-var User *user     //value is nil
-var liste []string //value is nill
-*/
-
 func main() {
+	var number int //decalaration must be typed, value is 0
+	fmt.Println("number:", number)
+	var number2 = 2 //assigned - no need to write down type
+	fmt.Println("number2:", number2)
+
+	var firstname string = "James"
+	var name = "Kirk"
+	fmt.Println("Fullname:", firstname, name)
+
+	var pointer *string //value is nil
+	fmt.Println("pointer (*):", pointer)
+	var list []string //value is []
+	fmt.Println("list[]:", list)
+
 	var ( // variables can be grouped
 		home   = os.Getenv("HOME")
 		user   = os.Getenv("USER")
@@ -23,7 +28,17 @@ func main() {
 	fmt.Println("Home:" + home)
 	fmt.Println("User:" + user)
 	fmt.Println("GoPath:" + gopath)
-	test := "Halloa" //short notation
-	const test2 = "World"
-	fmt.Println(test + " " + test2)
+	test := "Halloa"      //short notation
+	const test2 = "World" //constants are untyped by default
+	fmt.Println(test, test2)
+
+	const (
+		monday = iota //auto-counter - starts for each group of const at 0
+		tuesday
+		_ //dont use 2
+		thursday
+	)
+	fmt.Println("monday", monday)
+	fmt.Println("tuesday", tuesday)
+	fmt.Println("thursday", thursday)
 }
